0de53334854abdce7ddb9c6bbf3e47bcb9dabefa
Praveen Kumar chebrolu <praveenchebrolu18@gmail.com>
2025-07-17 23:05:14 -0700

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,33 +1,18 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-// Hash password before saving
+  name: String,
+  roles: { type: String,roles:['user','provider','admin'], default: 'user' },
+  email: { type: String, unique: true },
+  password: String,
+  googleId: String, // For Google OAuth
+ 
+  // Hash password before saving
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-const User = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
 
-module.exports = User;
